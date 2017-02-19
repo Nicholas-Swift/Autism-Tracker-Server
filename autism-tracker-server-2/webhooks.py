e5aa0e7cce1f1b83bdc4e7079cab931e10ed987f
Nicholas Swift <nicholas.w.swift@gmail.com>
2017-02-18 20:48:41 -0800

    Added webhook get

--- autism-tracker-server-2/webhooks.py
+++ autism-tracker-server-2/webhooks.py
@@ -7,8 +7,12 @@ import database
 # MARK: Active Hooks
 
 class ActiveHookHandler(webapp2.RequestHandler): # /active/events
+    def get(self):
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.write(json.dumps(database.webhookdata))
+
     def post(self):
         print(self.request.POST)
-        database.webhookdata.append(self.request.POST)
+        database.webhookdata.append(dict(self.request.POST))
         print(database.webhookdata)
         self.response.write("")
